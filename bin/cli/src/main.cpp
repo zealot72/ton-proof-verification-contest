70b5ff7162501568a799b2c9d10ed650dfda52b6
zealot72 <zealot72@protonmail.com>
2021-08-24 21:28:44 +0700

    split cli functionality to separate functions

--- bin/cli/src/main.cpp
+++ bin/cli/src/main.cpp
@@ -48,51 +48,134 @@
 #include <nil/crypto3/marshalling/types/zk/r1cs_gg_ppzksnark/proof.hpp>
 #include <nil/crypto3/marshalling/types/zk/r1cs_gg_ppzksnark/verification_key.hpp>
 
+#include "detail/risk_component.hpp"
+#include "csv.cpp"
+
 using namespace nil::crypto3;
 using namespace nil::crypto3::marshalling;
 using namespace nil::crypto3::zk;
 
-template<typename FieldType>
-components::blueprint<FieldType> test_disjunction_component(size_t w) {
-    
-    using field_type = FieldType;
+typedef algebra::curves::bls12<381> curve_type;
+typedef typename curve_type::scalar_field_type scalar_field_type;
+using Endianness = nil::marshalling::option::big_endian;
+typedef zk::snark::r1cs_gg_ppzksnark<curve_type> scheme_type;
 
-    std::size_t n = std::log2(w) + 
-        ((w > (1ul << std::size_t(std::log2(w))))? 1 : 0);
 
-    components::blueprint<field_type> bp;
-    components::blueprint_variable<field_type> output;
-    output.allocate(bp);
-
-    bp.set_input_sizes(1);
-
-    components::blueprint_variable_vector<field_type> inputs;
-    inputs.allocate(bp, n);
-
-    components::disjunction<field_type> d(bp, inputs, output);
-    d.generate_r1cs_constraints();
-
-    for (std::size_t j = 0; j < n; ++j) {
-        bp.val(inputs[j]) = typename field_type::value_type((w & (1ul << j)) ? 1 : 0);
-    }
-
-    d.generate_r1cs_witness();
-
-    assert(bp.val(output) == (w ? field_type::value_type::one() : field_type::value_type::zero()));
-    assert(bp.is_satisfied());
-
-    return bp;
+std::vector<std::uint8_t> load_file(boost::filesystem::path path) {
+    boost::filesystem::ifstream stream(path, std::ios::in | std::ios::binary);
+    auto eos = std::istreambuf_iterator<char>();
+    auto buffer = std::vector<uint8_t>(std::istreambuf_iterator<char>(stream), eos);
+    return buffer;
 }
 
+void save_file(
+    boost::filesystem::path path,
+    std::vector<std::uint8_t> blob
+) {
+    boost::filesystem::ofstream out(path);
+    for (const auto &v : blob) {
+        out << v;
+    }
+    out.close();
+}
+
+bool generate_keys(
+    boost::filesystem::path pk_path,
+    boost::filesystem::path vk_path
+) {
+    blueprint<scalar_field_type> bp;
+    contest::risk_reporting_component<scalar_field_type> contest_component(bp, 100);
+    contest_component.generate_r1cs_constraints();
+
+    const r1cs_constraint_system<scalar_field_type> constraint_system = bp.get_constraint_system();
+
+    scheme_type::keypair_type keypair = generate<scheme_type>(constraint_system);
+
+    std::vector<std::uint8_t> proving_key_byteblob =
+        nil::marshalling::verifier_input_serializer_tvm<scheme_type>::process(keypair.first);
+    std::vector<std::uint8_t> verification_key_byteblob =
+        nil::marshalling::verifier_input_serializer_tvm<scheme_type>::process(keypair.second);
+
+    save_file(pk_path, proving_key_byteblob);
+    std::cout << "prooving key saved to " << pk_path << std::endl;
+
+    save_file(vk_path, verification_key_byteblob);
+    std::cout << "verification key saved to " << vk_path << std::endl;
+
+    return true;
+}
+
+
+bool generate_proof(
+    boost::filesystem::path proof_path,
+    boost::filesystem::path pk_path,
+    std::vector<float> weights,
+    std::vector<float> risks,
+    float min_risk,
+    float max_risk
+) {
+    std::vector<std::uint8_t> proving_key_byteblob = load_file(pk_path);
+    nil::marshalling::status_type provingProcessingStatus = nil::marshalling::status_type::success;
+    typename scheme_type::proving_key_type pk = nil::marshalling::verifier_input_deserializer_tvm<scheme_type>::proving_key_process(
+        proving_key_byteblob.cbegin(),
+        proving_key_byteblob.cend(),
+        provingProcessingStatus);
+
+    blueprint<scalar_field_type> bp;
+    contest::risk_reporting_component<scalar_field_type> contest_component(bp, 100);
+    contest_component.generate_r1cs_constraints();
+    contest_component.generate_r1cs_witness(weights, risks, min_risk, max_risk);
+
+    std::cout << "Circuit satisfied: " << bp.is_satisfied() << std::endl;
+    if (!bp.is_satisfied()) {
+        return false;
+    }
+
+    const scheme_type::proof_type proof = prove<scheme_type>(pk, bp.primary_input(), bp.auxiliary_input());
+
+    std::vector<std::uint8_t> proof_byteblob =
+        nil::marshalling::verifier_input_serializer_tvm<scheme_type>::process(proof);
+
+    std::cout << "proof is saved to " << proof_path << std::endl;
+    save_file(proof_path, proof_byteblob);
+
+    return true;
+}
+
+
+bool verify_proof(
+    boost::filesystem::path proof_path,
+    boost::filesystem::path vk_path,
+    std::vector<float> risks,
+    float min_risk,
+    float max_risk
+) {
+    std::vector<std::uint8_t> proof_byteblob = load_file(proof_path);
+    nil::marshalling::status_type proofProcessingStatus = nil::marshalling::status_type::success;
+    typename scheme_type::proof_type proof = nil::marshalling::verifier_input_deserializer_tvm<scheme_type>::proof_process(
+        proof_byteblob.cbegin(),
+        proof_byteblob.cend(),
+        proofProcessingStatus);
+
+    std::vector<std::uint8_t> verification_key_byteblob = load_file(vk_path);
+    nil::marshalling::status_type verificationProcessingStatus = nil::marshalling::status_type::success;
+    typename scheme_type::verification_key_type vk = nil::marshalling::verifier_input_deserializer_tvm<scheme_type>::verification_key_process(
+        verification_key_byteblob.cbegin(),
+        verification_key_byteblob.cend(),
+        verificationProcessingStatus );
+
+    r1cs_primary_input<scalar_field_type> input = contest::get_public_input<scalar_field_type>(risks, min_risk, max_risk);
+    using basic_proof_system = r1cs_gg_ppzksnark<curve_type>;
+    const bool verified = verify<basic_proof_system>(vk, input, proof);
+    std::cout << "proof verified " << verified << std::endl;
+
+    return verified;
+}
+
+
 int main(int argc, char *argv[]) {
-
-    typedef algebra::curves::bls12<381> curve_type;
-    typedef typename curve_type::scalar_field_type scalar_field_type;
-    using Endianness = nil::marshalling::option::big_endian;
-
-    typedef zk::snark::r1cs_gg_ppzksnark<curve_type> scheme_type;
-
-    std::size_t num_constraints = 1000, input_size = 100;
+    float min_risk, max_risk;
+    boost::filesystem::path weight_path, risks_path;
 
     boost::filesystem::path pout, pkout, vkout, piout, viout;
     boost::program_options::options_description options(
@@ -101,14 +184,19 @@ int main(int argc, char *argv[]) {
     // clang-format off
     options.add_options()("help,h", "Display help message")
     ("version,v", "Display version")
-    ("generate", "Generate proofs and/or keys")
+    ("generate", "Generate private and verification keys")
     ("verify", "verify proofs and/or keys")
+    ("proof", "verify proofs and/or keys")
     ("proof-output,po", boost::program_options::value<boost::filesystem::path>(&pout)->default_value("proof"))
     ("primary-input-output,pio", boost::program_options::value<boost::filesystem::path>(&piout)->default_value
 ("pinput"))
     ("proving-key-output,pko", boost::program_options::value<boost::filesystem::path>(&pkout)->default_value("pkey"))
     ("verifying-key-output,vko", boost::program_options::value<boost::filesystem::path>(&vkout)->default_value("vkey"))
-    ("verifier-input-output,vio", boost::program_options::value<boost::filesystem::path>(&viout)->default_value("vio"));
+    ("verifier-input-output,vio", boost::program_options::value<boost::filesystem::path>(&viout)->default_value("vio"))
+    ("min-risk,x", boost::program_options::value<float>(&min_risk)->default_value(0))
+    ("max-risk,y", boost::program_options::value<float>(&max_risk)->default_value(100))
+    ("weights,w", boost::program_options::value<boost::filesystem::path>(&weight_path)->default_value("data/weights.csv"))
+    ("risks,r", boost::program_options::value<boost::filesystem::path>(&risks_path)->default_value("data/risks.csv"));
     // clang-format on
 
     boost::program_options::variables_map vm;
@@ -118,132 +206,20 @@ int main(int argc, char *argv[]) {
     if (vm.count("help") || argc < 2) {
         std::cout << options << std::endl;
         return 0;
-    }
-
-    std::cout << "Blueprint generation started." << std::endl;
-
-    components::blueprint<scalar_field_type> bp = sha2_two_to_one_bp<scalar_field_type>();
-
-    std::cout << "Blueprint generation finished." << std::endl;
-
-    std::cout << "R1CS generation started." << std::endl;
-
-    std::cout << "R1CS generation finished." << std::endl;
-
-    std::cout << "Starting generator" << std::endl;
-
-    typename scheme_type::keypair_type keypair = zk::snark::generate<scheme_type>(bp.get_constraint_system());
-
-    std::cout << "Starting prover" << std::endl;
-
-    const typename scheme_type::proof_type proof =
-        zk::snark::prove<scheme_type>(keypair.first, bp.primary_input(), bp.auxiliary_input());
-    
-    using verification_key_marshalling_type = types::r1cs_gg_ppzksnark_verification_key<
-        nil::marshalling::field_type<
-            Endianness>,
-        typename scheme_type::verification_key_type>;
-
-    verification_key_marshalling_type filled_verification_key_val = 
-        types::fill_r1cs_gg_ppzksnark_verification_key<
-            typename scheme_type::verification_key_type,
-            Endianness>(keypair.second);
-
-    using proof_marshalling_type = types::r1cs_gg_ppzksnark_proof<
-        nil::marshalling::field_type<
-            Endianness>,
-        typename scheme_type::proof_type>;
-
-    proof_marshalling_type filled_proof_val = 
-        types::fill_r1cs_gg_ppzksnark_proof<
-            typename scheme_type::proof_type,
-            Endianness>(proof);
-
-    using primary_input_marshalling_type = types::r1cs_gg_ppzksnark_primary_input<
-        nil::marshalling::field_type<
-            Endianness>,
-        typename scheme_type::primary_input_type>;
-
-    primary_input_marshalling_type filled_primary_input_val = 
-        types::fill_r1cs_gg_ppzksnark_primary_input<
-            typename scheme_type::primary_input_type,
-            Endianness>(bp.primary_input());
-
-    std::cout << "Marshalling types filled." << std::endl;
-
-    using unit_type = unsigned char;
-
-    std::vector<unit_type> verification_key_byteblob;
-    verification_key_byteblob.resize(filled_verification_key_val.length(), 0x00);
-    auto write_iter = verification_key_byteblob.begin();
-
-    typename nil::marshalling::status_type status =  
-        filled_verification_key_val.write(write_iter, 
-            verification_key_byteblob.size());
-
-    std::vector<unit_type> proof_byteblob;
-    proof_byteblob.resize(filled_proof_val.length(), 0x00);
-    write_iter = proof_byteblob.begin();
-
-    status = filled_proof_val.write(write_iter, 
-            proof_byteblob.size());
-
-    std::vector<unit_type> primary_input_byteblob;
-
-    primary_input_byteblob.resize(filled_primary_input_val.length(), 0x00);
-    auto primary_input_write_iter = primary_input_byteblob.begin();
-
-    status = filled_primary_input_val.write(primary_input_write_iter, 
-            primary_input_byteblob.size());
-
-    std::cout << "Byteblobs filled." << std::endl;
-
-    if (vm.count("verifying-key-output")) {
-        boost::filesystem::ofstream out(vkout);
-        for (const auto &v : verification_key_byteblob) {
-            out << v;
+    } else if (vm.count("generate")) {
+        generate_keys(pkout, vkout);
+    } else if (vm.count("proof") || vm.count("verify")) {
+        // TODO read maps from csv files
+        // std::vector<std::vector<std::string>> table = readCSV("data.csv");
+        std::map<std::string, int> weights_map;
+        std::map<std::string, int> risks_map;
+        // TODO: trasform maps to arrays with fixed order
+        std::vector<float> weights, risks;
+        if (vm.count("proof")) {
+            generate_proof(pout, pkout, weights, risks, min_risk, max_risk);
+        } else if (vm.count("verify")) {
+            verify_proof(pout, vkout, risks, min_risk, max_risk);
         }
-        out.close();
-    }
-
-    if (vm.count("proof-output")) {
-        boost::filesystem::ofstream out(pout);
-        for (const auto &v : proof_byteblob) {
-            out << v;
-        }
-        out.close();
-    }
-
-    if (vm.count("primary-input-output")) {
-        boost::filesystem::ofstream out(piout);
-        for (const auto &v : primary_input_byteblob) {
-            out << v;
-        }
-        out.close();
-    }
-
-    // nil::marshalling::status_type provingProcessingStatus = nil::marshalling::status_type::success;
-    // typename scheme_type::proving_key_type other =
-    //             nil::marshalling::verifier_input_deserializer_tvm<scheme_type>::proving_key_process(
-    //                 proving_key_byteblob.cbegin(),
-    //                 proving_key_byteblob.cend(),
-    //                 provingProcessingStatus);
-
-    // assert(keypair.first == other);
-
-    if (vm.count("verifier-input-output")) {
-        std::vector<std::uint8_t> verifier_input_output_byteblob(proof_byteblob.begin(), proof_byteblob.end());
-
-        verifier_input_output_byteblob.insert(verifier_input_output_byteblob.end(), primary_input_byteblob.begin(),
-                                              primary_input_byteblob.end());
-        verifier_input_output_byteblob.insert(verifier_input_output_byteblob.end(), verification_key_byteblob.begin(),
-                                              verification_key_byteblob.end());
-
-        boost::filesystem::ofstream poutf(viout);
-        for (const auto &v : verifier_input_output_byteblob) {
-            poutf << v;
-        }
-        poutf.close();
     }
 
     return 0;
